85ba9e872e9ab4db2dcc695e25598e588226c1b9
BalanceSeeker56 <wielgosz.mac@gmail.com>
2022-07-12 06:47:04 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const showBtn = document.querySelector('.sectionsBtn')
+
+
+showBtn.addEventListener('click', () => {
+    const sectionHide = document.querySelector('.hide')
+    if(sectionHide.style.display === 'none' || sectionHide.style.display === "") {
+    sectionHide.style.display = 'block';
+    showBtn.textContent = "Ukryj szczegóły";
+    } else {
+    sectionHide.style.display = 'none';
+    showBtn.textContent = "Pokaż szczegóły";
+    }
+});
